2bed0f709f659f37ee1ebef60f04d88d7e70fc79
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2023-02-16 16:21:13 -0300

    feat: requisito 1

---
+++ src/Races/Race.ts
@@ -0,0 +1,26 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name():string {
+    return this.name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances() {
+    const error = new Error('Not implemented');
+    throw error;
+  }
+
+  abstract maxLifePoints():number;
+}
+
+export default Race;
